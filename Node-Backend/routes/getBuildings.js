e4aa74fec9c29a0e5d1a38f1126fa4fe326dd9dd
Bhavesh Yadav <ybhavesh0915@gmail.com>
2026-05-01 17:01:56 +0530

    Delete Node-Backend directory

--- Node-Backend/routes/getBuildings.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const getBuilding = require('../controllers/getBuildingController');
-
-router.get('/building/:id', getBuilding);
-
-module.exports = router;
